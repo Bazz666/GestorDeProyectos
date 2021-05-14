6413562cf6599a865ed233bb9ff0e0569ee32cd0
bazz666 <herrera.se1992@gmail.com>
2021-05-14 19:26:55 -0400

    final commit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,18 @@
 class ProjectsController < ApplicationController
+  http_basic_authenticate_with name: "desafiovamoscontodo", password: "XAHTJEAS23123%23", only: :dashboard
+
   def index
   end
 
-  def dashboard
+  def dashboard #aplicando el filtro por estado
+    if params[:state].present?
+      @projects = Project.where("state = ?", params[:state])
+    else
+      @projects = Project.all
+    end
   end
 
   def created
+    @project = Project.create(name: params[:name], description: params[:description], start: params[:start], end: params[:end], state: params[:state])
   end
 end
