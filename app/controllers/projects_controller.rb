50a1d2a592b8ba979969904a7b7eba3033c7152d
bazz666 <herrera.se1992@gmail.com>
2021-05-14 18:14:00 -0400

    add and edit views, metods and controllers

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,10 @@
+class ProjectsController < ApplicationController
+  def index
+  end
+
+  def dashboard
+  end
+
+  def created
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'projects/index'
+  get 'projects/dashboard'
+  get 'projects/created'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
